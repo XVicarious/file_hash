8255fe7891c78cd3d4585f72ee50d38221a1c0a7
Brian Maurer <bmaurer@xvicario.us>
2018-10-27 23:06:02 -0400

    fixed a couple things

--- file_hash.py
+++ file_hash.py
@@ -14,11 +14,11 @@ PLUGIN_ID = 'file_hash'
 
 log = logging.getLogger(PLUGIN_ID)
 
-size_units = object
-size_units.B = 1
-size_units.KB = size_units.B * 1024
-size_units.MB = size_units.KB * 1024
-size_units.GB = size_units.MB * 1024
+# Please forgive me for this
+B = 1
+KiB = B * 1024
+MiB = KiB * 1024
+GiB = MiB * 1024
 
 
 class FileHashPlugin(object):
@@ -56,13 +56,13 @@ class FileHashPlugin(object):
             current_hasher = hasher.copy()
             with open(entry['location'], 'rb') as to_hash:
                 to_hash_size = to_hash.tell()
-                hundered_meg = size_units.MB * 100
-                total_pieces = math.ciel(to_hash_size / hundered_meg)
+                hundered_meg = MiB * 100
+                total_pieces = math.ceil(to_hash_size / hundered_meg)
                 i = 0
                 while True:
                     i += 1
                     log.debug('Hashing piece: %s/%s', i, total_pieces)
-                    piece = to_hash.read(size_units.MB * 100)
+                    piece = to_hash.read(hundered_meg)
                     if not piece:
                         break
                     current_hasher.update(piece)
